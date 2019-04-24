a8ef7ea1048d7e7268f82ead625e090e44e3732c
xingkongs <ldk8259878@163.com>
2019-04-24 15:42:03 +0800

    创建开发和生产两个config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    mode: "production",
+    // mode: "production",
     entry: {
         index: "./lib/index.tsx"
     },
@@ -21,24 +20,24 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: "xrUI",
-            template: "index.html"
-        })
-    ],
-    externals: {
-        react: {
-            commonjs: "react",
-            commonjs2: "react",
-            amd: "react",
-            root: "React",
-        },
-        "react-dom": {
-            commonjs: "react-dom",
-            commonjs2: "react-dom",
-            amd: "react-dom",
-            root: "ReactDOM",
-        },
-    },
+    // plugins: [
+    //     new HtmlWebpackPlugin({
+    //         title: "xrUI",
+    //         template: "index.html"
+    //     })
+    // ],
+    // externals: {
+    //     react: {
+    //         commonjs: "react",
+    //         commonjs2: "react",
+    //         amd: "react",
+    //         root: "React",
+    //     },
+    //     "react-dom": {
+    //         commonjs: "react-dom",
+    //         commonjs2: "react-dom",
+    //         amd: "react-dom",
+    //         root: "ReactDOM",
+    //     },
+    // },
 };
