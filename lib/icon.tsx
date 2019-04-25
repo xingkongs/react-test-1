2e1976f7c0244f9920d4a4068952bb794ae46b7e
xingkongs <ldk8259878@163.com>
2019-04-25 12:33:39 +0800

    动态引入svg

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import "../icons/wechat.svg";
-import "../icons/qq.svg";
-import "../icons/alipay.svg";
+import "./importAllIcons"
 interface IconProps {
     name: string
 }
