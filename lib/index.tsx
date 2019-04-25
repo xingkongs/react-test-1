f033ac6cbecb5adcc29e86cfb1561b4c9c0f30d3
xingkongs <ldk8259878@163.com>
2019-04-25 13:45:40 +0800

    为e定义类型

--- lib/index.tsx
+++ lib/index.tsx
@@ -2,9 +2,9 @@ import Icon from "./icon";
 import ReactDOM from "react-dom";
 import React from "react";
 // @ts-ignore
-const fn = ()=>{
-    console.log("fn");
-}
+const fn: React.MouseEventHandler<SVGElement> = (e) => {
+    console.log(e.target);
+};
 ReactDOM.render(<div>
     <Icon name='wechat' onClick={fn}/>
 </div>, document.body.querySelector("#root"));
