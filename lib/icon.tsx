c855dd188043f061771b957c961f41b4534e8824
xingkongs <ldk8259878@163.com>
2019-04-25 11:46:59 +0800

    Icon成功

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import wechat from "../icons/wechat.svg"
-console.log(wechat)
+import "../icons/wechat.svg"
 interface IconProps {
     name:string
 }
 const Icon:React.FunctionComponent<IconProps> = (props)=>{
     return (
         <span>
-            {props.name}
+            <svg>
+                <use xlinkHref={`#${props.name}`}/>
+            </svg>
         </span>
     );
 }
