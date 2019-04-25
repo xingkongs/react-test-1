41babd24866279a35c2e3c484aaf29f16610b8de
xingkongs <ldk8259878@163.com>
2019-04-25 15:06:28 +0800

    将icon移动到单独文件夹

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,4 +1,4 @@
-import Icon from "./icon";
+import Icon from "./icon/icon";
 import ReactDOM from "react-dom";
 import React from "react";
 // @ts-ignore
@@ -7,4 +7,6 @@ const fn: React.MouseEventHandler<SVGElement> = (e) => {
 };
 ReactDOM.render(<div>
     <Icon name='wechat' onClick={fn} onMouseEnter={() => {console.log("enter");}} onMouseLeave={() => {console.log("leave");}}/>
+    <Icon name='qq'/>
+    <Icon name='alipay'/>
 </div>, document.body.querySelector("#root"));
