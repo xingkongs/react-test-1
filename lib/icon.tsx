cb956f892655dd5ef66c39e2a0833aa430cc3eee
xingkongs <ldk8259878@163.com>
2019-04-25 10:57:43 +0800

    声明函数组件Icon 接收的props类型 （不忽略node_modu..）

---
+++ lib/icon.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface IconProps {
+    name:string
+}
+const Icon:React.FunctionComponent<IconProps> = ()=>{
+    return (
+        <span>
+            span
+        </span>
+    );
+}
+export default Icon;
--- lib/index.tsx
+++ lib/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Button from './button'
-
-ReactDOM.render(<Button></Button>,document.body.querySelector('#root'))
+import Icon from './icon'
+import ReactDOM from 'react-dom'
+import React from 'react'
+// @ts-ignore
+ReactDOM.render(<div>
+    <Icon name='wechat'></Icon>
+</div>,document.body.querySelector('#root'))
