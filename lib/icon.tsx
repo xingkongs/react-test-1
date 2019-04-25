4005de25c81f73756a4aa7bbfef3c1c849fa01aa
xingkongs <ldk8259878@163.com>
2019-04-25 14:12:12 +0800

    用户传了className

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,16 +1,14 @@
 import React from "react";
-import "./importAllIcons"
-import './icon.scss'
-interface IconProps extends React.SVGAttributes<SVGElement>{
+import "./importAllIcons";
+import "./icon.scss";
+interface IconProps extends React.SVGAttributes<SVGElement> {
     name: string
 }
 const Icon: React.FunctionComponent<IconProps> = (props) => {
-    const {className,...restProps} = props
+    const {className, ...restProps} = props;
     return (
         <span>
-            <svg className={`xrui-icon ${className}` }
-            {...restProps}
-            >
+            <svg className={`xrui-icon ${className}`}{...restProps}>
                 <use xlinkHref={`#${props.name}`}/>
             </svg>
         </span>
