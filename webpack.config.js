7b92f5b837ccd7d35e9b1ef294157038c7f14684
xingkongs <ldk8259878@163.com>
2019-04-25 11:15:01 +0800

    更正typescript rule

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /.tsx?$/,
+                test: /\.tsx?$/,
                 loader: "awesome-typescript-loader"
             }
         ]
