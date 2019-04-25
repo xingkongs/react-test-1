3b389a850bf5edb54fe2c4c285a1d0a0fe365944
xingkongs <ldk8259878@163.com>
2019-04-25 13:20:46 +0800

    引入scss文件 安装node-sass

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./importAllIcons"
+import './icon.scss'
 interface IconProps {
     name: string
 }
 const Icon: React.FunctionComponent<IconProps> = (props) => {
     return (
         <span>
-            <svg>
+            <svg className="icon">
                 <use xlinkHref={`#${props.name}`}/>
             </svg>
         </span>
