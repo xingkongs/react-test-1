3cb4ab3694df990150042ae6315b6571a25366ff
xingkongs <ldk8259878@l63.com>
2019-04-30 15:06:03 +0800

    将button移动到一个文件夹

--- example.tsx
+++ example.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {HashRouter as Router, Route, Link} from "react-router-dom";
 import Icon from "./lib/icon/icon.example";
-import ButtonExample from "./lib/button.example";
+import ButtonExample from "./lib/button/button.example";
 ReactDOM.render(
     <Router>
         <div>
