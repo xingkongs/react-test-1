5181fdcc3f208388b73f26df9763e984b6bf39a1
xingkongs <ldk8259878@l63.com>
2019-04-30 14:45:25 +0800

    添加简易官网

--- example.tsx
+++ example.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Icon from "./lib/icon/icon";
+import {HashRouter as Router, Route, Link} from "react-router-dom";
+import Icon from "./lib/icon/icon.example";
+import ButtonExample from "./lib/button.example";
 ReactDOM.render(
-    <Icon name="alipay"/>,
+    <Router>
+        <div>
+            <header>
+                XRUI
+            </header>
+            <div>
+                <aside>
+                    <h2>组件</h2>
+                    <ul>
+                        <li><Link to="/icon">Icon</Link></li>
+                        <li><Link to="/button">Button</Link></li>
+                    </ul>
+                </aside>
+                <main>
+                    <Route path="/icon" component={Icon}/> <Route path="/button" component={ButtonExample}/>
+                </main>
+            </div>
+        </div>
+    </Router>,
     document.querySelector("#root")
 );
