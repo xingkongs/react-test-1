0d7f05d8cf7fc65f3233c585e93c45ff3cc6fc6c
xingkongs <ldk8259878@l63.com>
2019-04-30 14:08:26 +0800

    添加 example示例 添加dev entry tsconfig 添加example files option

---
+++ example.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Icon from "./lib/icon/icon";
+ReactDOM.render(
+    <Icon name="alipay"/>,
+    document.querySelector("#root")
+);
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
     // plugins: [
     //     new HtmlWebpackPlugin({
     //         title: "xrUI",
-    //         template: "index.html"
+    //         template: "example.html"
     //     })
     // ],
     // externals: {
