aff18b3d1d4850b70eb43b189634b409f9fda2a0
xingkongs <ldk8259878@l63.com>
2019-05-05 16:42:58 +0800

    dialog 用户需要传入 buttons 以及关闭函数

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,9 +1,13 @@
+import Button from "../button/button";
 const dialogExample = () => {
     const [x, setX] = useState(false);
     return (
         <div>
-            <button onClick={()=>{setX(!x)}}>click me</button>
-            <Dialog visible={x}>
+            <button onClick={() => {setX(!x);}}>click me</button>
+            <Dialog visible={x} buttons={[
+                <Button value="ok" onClick={() => setX(false)}/>,
+                <Button value="cancle" onClick={() => {setX(false);}}/>
+            ]} onClose={() => {setX(false);}}>
                 <div>dialog</div>
             </Dialog>
         </div>
