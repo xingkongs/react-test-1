c9a933dea5e5c42a83297340c7a1cce8293ded80
xingkongs <ldk8259878@l63.com>
2019-05-05 14:09:04 +0800

    添加dialog example

--- example.tsx
+++ example.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {HashRouter as Router, Route, Link} from "react-router-dom";
-import Icon from "./lib/icon/icon.example";
+import IconExample from "./lib/icon/icon.example";
 import ButtonExample from "./lib/button/button.example";
+import DialogExample from "./lib/dialog/dialog.example";
 ReactDOM.render(
     <Router>
         <div>
@@ -15,10 +16,12 @@ ReactDOM.render(
                     <ul>
                         <li><Link to="/icon">Icon</Link></li>
                         <li><Link to="/button">Button</Link></li>
+                        <li><Link to="/dialog">Dialog</Link></li>
                     </ul>
                 </aside>
                 <main>
-                    <Route path="/icon" component={Icon}/> <Route path="/button" component={ButtonExample}/>
+                    <Route exact path="/" component={IconExample}/> <Route path="/icon" component={IconExample}/>
+                    <Route path="/button" component={ButtonExample}/> <Route path="/dialog" component={DialogExample}/>
                 </main>
             </div>
         </div>
---
+++ lib/dialog/dialog.example.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+const dialogExample = () => {
+    return (
+        <div>dialog</div>
+    );
+};
+export default dialogExample;
