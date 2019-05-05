55cffa2e68ba2c3b488a055a78b52bc869d3ee2f
xingkongs <ldk8259878@l63.com>
2019-05-05 15:05:41 +0800

    dialog 添加close svg

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,5 +1,6 @@
 import React, {Fragment} from "react";
 import "./dialog.scss";
+import Icon from "../icon/icon";
 interface Props extends React.DOMAttributes<Element> {
     visible: boolean
 }
@@ -8,7 +9,19 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
         props.visible ?
             <Fragment>
                 <div className="xrui-dialog-mask"/>
-                <div className="xrui-dialog">{props.children}</div>
+                <div className="xrui-dialog">
+                    <div className="xrui-dialog-close">
+                        <Icon name="close"/>
+                    </div>
+                    <header className="xrui-dialog-header">title</header>
+                    <main className="xrui-dialog-main">
+                        {props.children}
+                    </main>
+                    <footer className="xrui-dialog-footer">
+                        <button>ok</button>
+                        <button>cancle</button>
+                    </footer>
+                </div>
             </Fragment>
             : null
     );
