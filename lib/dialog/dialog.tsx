a7db9c586a2718852e3d475058c22630eb7e17ee
xingkongs <ldk8259878@l63.com>
2019-05-05 15:22:30 +0800

    dialog 抽离scopedClassMaker函数

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,29 +1,26 @@
 import React, {Fragment} from "react";
 import "./dialog.scss";
 import Icon from "../icon/icon";
+import {scopedClassMaker} from "../helpers/scopedClass";
 interface Props extends React.DOMAttributes<Element> {
     visible: boolean
 }
-function y(prefix: string) {
-    return function (name?: string) {
-        return [prefix, name].filter(Boolean).join("-");
-    };
-}
-const x = y("xrui-dialog");
+const scopedClass = scopedClassMaker("xrui-dialog");
+const sc = scopedClass;
 const Dialog: React.FunctionComponent<Props> = (props) => {
     return (
         props.visible ?
             <Fragment>
-                <div className={x("mask")}/>
-                <div className={x()}>
-                    <div className={x("close")}>
+                <div className={sc("mask")}/>
+                <div className={sc()}>
+                    <div className={sc("close")}>
                         <Icon name="close"/>
                     </div>
-                    <header className={x("header")}>title</header>
-                    <main className={x("main")}>
+                    <header className={sc("header")}>title</header>
+                    <main className={sc("main")}>
                         {props.children}
                     </main>
-                    <footer className={x("footer")}>
+                    <footer className={sc("footer")}>
                         <button>ok</button>
                         <button>cancle</button>
                     </footer>
