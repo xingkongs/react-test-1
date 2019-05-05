131fbac9866c7fbd5c8213c6495fd8d25cd0c303
xingkongs <ldk8259878@l63.com>
2019-05-05 15:15:20 +0800

    dialog 添加class 帮助函数

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -4,20 +4,26 @@ import Icon from "../icon/icon";
 interface Props extends React.DOMAttributes<Element> {
     visible: boolean
 }
+function y(prefix: string) {
+    return function (name?: string) {
+        return [prefix, name].filter(Boolean).join("-");
+    };
+}
+const x = y("xrui-dialog");
 const Dialog: React.FunctionComponent<Props> = (props) => {
     return (
         props.visible ?
             <Fragment>
-                <div className="xrui-dialog-mask"/>
-                <div className="xrui-dialog">
-                    <div className="xrui-dialog-close">
+                <div className={x("mask")}/>
+                <div className={x()}>
+                    <div className={x("close")}>
                         <Icon name="close"/>
                     </div>
-                    <header className="xrui-dialog-header">title</header>
-                    <main className="xrui-dialog-main">
+                    <header className={x("header")}>title</header>
+                    <main className={x("main")}>
                         {props.children}
                     </main>
-                    <footer className="xrui-dialog-footer">
+                    <footer className={x("footer")}>
                         <button>ok</button>
                         <button>cancle</button>
                     </footer>
