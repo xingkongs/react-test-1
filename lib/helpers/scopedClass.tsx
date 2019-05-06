ec9b4f714df4deb82b91ea9cdef048bca4957275
xingkongs <ldk8259878@l63.com>
2019-05-06 16:13:22 +0800

    重构scopedClassMaker 第一个参数接受value为boolean的对象

--- lib/helpers/scopedClass.tsx
+++ lib/helpers/scopedClass.tsx
@@ -1,9 +1,20 @@
 interface Options {
     extra: string | undefined
 }
+interface classToggles {
+    [k: string]: boolean
+}
 function scopedClassMaker(prefix: string) {
-    return function (name?: string, options?: Options) {
-        const result = [prefix, name].filter(Boolean).join("-");
+    return function (name?: string | classToggles, options?: Options) {
+        let name2, result;
+        if (typeof name === "string" || name === undefined) {
+            result = [prefix, name].filter(Boolean).join("-");
+        } else {
+            name2 = Object.entries(name).filter(km => km[1]).map(k => k[0]);
+            result = name2.map((n) => {
+                return [prefix, n].filter(Boolean).join("-");
+            }).join(" ");
+        }
         if (options && options.extra) {
             return [result, options.extra].join(" ");
         } else {
