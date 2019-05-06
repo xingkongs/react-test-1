301756b521b4f98fbe00cf3cd200791e5679904d
xingkong <ldk82598789@163.com>
2019-05-06 23:30:32 +0800

    官网基本完成

--- example.tsx
+++ example.tsx
@@ -5,14 +5,20 @@ import IconExample from "./lib/icon/icon.example";
 import ButtonExample from "./lib/button/button.example";
 import DialogExample from "./lib/dialog/dialog.example";
 import LayoutExample from "./lib/layout/layout.exampe";
+import {Layout, Aside, Header, Content, Footer} from "./lib/layout/layout";
+import "./example.scss";
+const logo = require("./logo.png");
 ReactDOM.render(
     <Router>
-        <div>
-            <header>
-                XRUI
-            </header>
-            <div>
-                <aside>
+        <Layout className="page">
+            <Header className="page-header">
+                <div className="wrapped">
+                    <img className="header-logo" src={logo} alt=""/>
+                    XRUI
+                </div>
+            </Header>
+            <Layout>
+                <Aside className="page-aside">
                     <h2>组件</h2>
                     <ul>
                         <li><Link to="/icon">Icon</Link></li>
@@ -20,14 +26,17 @@ ReactDOM.render(
                         <li><Link to="/dialog">Dialog</Link></li>
                         <li><Link to="/layout">Layout</Link></li>
                     </ul>
-                </aside>
-                <main>
+                </Aside>
+                <Content className="page-content">
                     <Route exact path="/" component={IconExample}/> <Route path="/icon" component={IconExample}/>
                     <Route path="/button" component={ButtonExample}/> <Route path="/dialog" component={DialogExample}/>
-                    <Route path="/layout" component={LayoutExample} />
-                </main>
-            </div>
-        </div>
+                    <Route path="/layout" component={LayoutExample}/>
+                </Content>
+            </Layout>
+            <Footer className="page-footer">
+                <div className="wrapped">footer</div>
+            </Footer>
+        </Layout>
     </Router>,
     document.querySelector("#root")
 );
