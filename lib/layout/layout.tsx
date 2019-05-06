427518f6d827b8d5af07a7deb60d3b5140554073
xingkongs <ldk8259878@l63.com>
2019-05-06 15:25:38 +0800

    遍历children 是否为Aside

--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./layout.scss";
 import {scopedClassMaker} from "../helpers/scopedClass";
+import Aside from "./aside";
 const sc = scopedClassMaker("xrui-layout");
 interface Props extends React.HTMLAttributes<HTMLElement> {
+    children: Array<React.ReactElement>
 }
 const Layout: React.FunctionComponent<Props> = (props) => {
     const {className, ...reset} = props;
+    let hasAside = false;
+    props.children.map(node => {
+        if (node.type === Aside) {
+            hasAside = true;
+        }
+    });
     return (
-        <div className={sc("", {extra: className})} {...reset}>
+        <div className={sc("", {extra: [className, hasAside && "xrui-hasAside"].join(" ")})} {...reset}>
             {props.children}
         </div>
     );
