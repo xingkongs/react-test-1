e1a115bd6ef08a7af0a54bd4f7429fa3a94fd4fe
xingkongs <ldk8259878@l63.com>
2019-05-07 11:43:08 +0800

    delete files

--- jest.config.ci.js
+++
@@ -1,8 +0,0 @@
-const base = require("./jest.config");
-module.exports = Object.assign({}, base, {
-    collectCoverage: true,
-    reporters: ["jest-junit"],
-    collectCoverageFrom: ["{lib,include}/**/*.{js,jsx,ts,tsx}", "!**/node_modules/**"],
-    coverageDirectory: "coverage",
-    coverageReporters: ["text", "lcov"],
-});
--- lib/button/__tests__/button.unit.tsx
+++
@@ -1,9 +0,0 @@
-import renderer from "react-test-renderer";
-import React from "react";
-import Button from "../button";
-describe("button", function () {
-    it("是一个div", () => {
-        const json = renderer.create(<Button value="button"/>).toJSON();
-        expect(json).toMatchSnapshot();
-    });
-});
