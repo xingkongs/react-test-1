cf68785e0bcd033dd3f0e42cb09ee2dbc33ae44f
xingkongs <ldk8259878@l63.com>
2019-12-12 16:53:32 +0800

    remove unnecessary example add calculated scrollbar height

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {UIEventHandler, useState, useRef, useEffect} from "react";
 import {scopedClassMaker} from "../helpers/classes";
 import "./scroll.scss";
 import scrollbarWidth from "./scroll-width";
@@ -10,10 +10,23 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
 const sc = scopedClassMaker("xrui-scroll");
 const Scroll: React.FunctionComponent<Props> = (props) => {
     const {children, ...reset} = props;
+    const [barHeight, setBarHeight] = useState(0);
+    let onScroll: UIEventHandler = (e) => {
+        console.log(e);
+    };
+    const containRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        const scrollHeight = containRef.current!.scrollHeight;
+        const viewHeight = containRef.current!.getBoundingClientRect().height;
+        setBarHeight(viewHeight * viewHeight / scrollHeight);
+    }, []);
     return (
         <div className={sc("")} {...reset}>
-            <div className={sc("inner")} style={{right: -scrollbarWidth()}}>
+            <div className={sc("inner")} style={{right: -scrollbarWidth()}} ref={containRef} onScroll={onScroll}>
                 {children}
+            </div>
+            <div className={sc("track")}>
+                <div className={sc("bar")} style={{height: barHeight}}/>
             </div>
         </div>
     );
